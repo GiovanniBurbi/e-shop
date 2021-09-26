f8da9f0c83c9ba139d21e2235e810221d7b14b05
Giovanni <giovanniburbi10@gmail.com>
2021-09-26 12:04:20 +0200

    MongoDB dependency and implemented GIVEN step First scenario

--- com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/DatabaseSteps.java
+++ com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/DatabaseSteps.java
@@ -1,13 +1,35 @@
 package com.apt.project.eshop.bdd.steps;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 
 public class DatabaseSteps {
 
+	static final String DB_NAME = "test-db";
+	static final String COLLECTION_NAME = "test-collection";
+	private MongoClient mongoClient;
+
+	@Before
+	public void setUp() {
+		mongoClient = new MongoClient();
+		// to start with an empty database
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+
 	@Given("The database contains a product with id {string}, name {string} and price {double}")
 	public void the_database_contains_a_product_with_id_name_and_price(String id, String name, Double price) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
+				.insertOne(new Document().append("id", id).append("name", name).append("price", price));
 	}
 	
 }
