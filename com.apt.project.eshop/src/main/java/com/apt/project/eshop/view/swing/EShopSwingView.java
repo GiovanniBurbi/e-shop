10dae94e5901e81d468e14cc60dddf94f888021e
Giovanni <giovanniburbi10@gmail.com>
2021-09-26 18:12:49 +0200

    Resolved Security Hotspots issue and View critical code smell

--- com.apt.project.eshop/src/main/java/com/apt/project/eshop/view/swing/EShopSwingView.java
+++ com.apt.project.eshop/src/main/java/com/apt/project/eshop/view/swing/EShopSwingView.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
 import com.apt.project.eshop.model.Product;
@@ -25,7 +26,7 @@ public class EShopSwingView extends JFrame implements EShopView{
 	 */
 	public EShopSwingView() {
 		setTitle("eShop View");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setBounds(100, 100, 829, 401);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
