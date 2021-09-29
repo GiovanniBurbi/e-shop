bd6d6c663ae26fdf52f32b83cf24dc3a6c892ec8
Giovanni <giovanniburbi10@gmail.com>
2021-09-29 16:36:39 +0200

    Fix: ViewSteps After method. Picocli refactoring

--- com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/EShopSwingViewSteps.java
+++ com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/EShopSwingViewSteps.java
@@ -10,8 +10,10 @@ import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
 
+import com.apt.project.eshop.bdd.EShopAppBDD;
 import com.apt.project.eshop.model.Product;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -19,10 +21,24 @@ public class EShopSwingViewSteps {
 
 	private FrameFixture window;
 
+	static final String DB_NAME = "test-db";
+	static final String COLLECTION_NAME = "test-collection";
+
+	@After
+	public void tearDown() {
+		if (window != null)
+			window.cleanUp();
+	}
+
 	@When("The eShop View is shown")
 	public void the_eShop_View_is_shown() {
 		// start the Swing application
-		application("com.apt.project.eshop.app.swing.EShopSwingApp").start();
+		application("com.apt.project.eshop.app.swing.EShopSwingApp").withArgs(
+				"--mongo-port=" + EShopAppBDD.mongoPort,
+				"--db-name=" + DB_NAME,
+				"--db-collection=" + COLLECTION_NAME
+
+		).start();
 		// get a reference of its JFrame
 		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
 			@Override
@@ -30,12 +46,13 @@ public class EShopSwingViewSteps {
 				return "eShop View".equals(frame.getTitle()) && frame.isShowing();
 			}
 
-		}).using(BasicRobot.robotWithCurrentAwtHierarchy());		
+		}).using(BasicRobot.robotWithCurrentAwtHierarchy());
 	}
 
 	@Then("The product list contains an element with id {string}, name {string} and price {double}")
 	public void the_product_list_contains_an_element_with_id_name_and_price(String id, String name, Double price) {
-		assertThat(window.list("productList").contents()).anySatisfy(e -> assertThat(e).contains(new Product(id, name, price).toString()));
+		assertThat(window.list("productList").contents())
+				.anySatisfy(e -> assertThat(e).contains(new Product(id, name, price).toString()));
 	}
-	
+
 }
--- com.apt.project.eshop/src/main/java/com/apt/project/eshop/app/swing/EShopSwingApp.java
+++ com.apt.project.eshop/src/main/java/com/apt/project/eshop/app/swing/EShopSwingApp.java
@@ -1,6 +1,7 @@
 package com.apt.project.eshop.app.swing;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,32 +12,55 @@ import com.apt.project.eshop.view.swing.EShopSwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class EShopSwingApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-	/**
-	 *  Start MongoDB with Docker with
-	 *
-	 * <pre>
-	 * docker run -p 27017:27017 --rm mongo:4.4.3
-	 * </pre>
-	 * 
-	 * Launch the application.
-	 * 
-	 */
+
+/**
+ *  Start MongoDB with Docker with
+ *
+ * <pre>
+ * docker run -p 27017:27017 --rm mongo:4.4.3
+ * </pre>
+ * 
+ * Launch the application.
+ */
+@Command(mixinStandardHelpOptions = true)
+public class EShopSwingApp implements Callable<Void> {
+	
+	@Option(names = { "--mongo-host" }, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+
+	@Option(names = { "--mongo-port" }, description = "MongoDB host port")
+	private int mongoPort = 27017;
+
+	@Option(names = { "--db-name" }, description = "Database name")
+	private String databaseName = "eShop";
+
+	@Option(names = { "--db-collection" }, description = "Collection name")
+	private String collectionName = "products";
+	
 	public static void main(String[] args) {
+		new CommandLine(new EShopSwingApp()).execute(args);
+	}
+	
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
-				MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
-				ProductMongoRepository productRepository = new ProductMongoRepository(client, "eShop", "products");
+				MongoClient client = new MongoClient(new ServerAddress(mongoHost, mongoPort));
+				ProductMongoRepository productRepository = new ProductMongoRepository(client, databaseName,	collectionName);
 				productRepository.loadCatalog(new Product("1", "Laptop", 1300));
 				EShopSwingView eShopView = new EShopSwingView();
 				EShopController eShopController = new EShopController(productRepository, eShopView);
 				eShopView.setVisible(true);
 				eShopController.allProducts();
 			} catch (Exception e) {
-				Logger.getLogger(EShopSwingApp.class.getName())
+				Logger.getLogger(getClass().getName())
 				.log(Level.SEVERE, "Exception", e);
 			}
 		});
+		return null;
 	}
 }
