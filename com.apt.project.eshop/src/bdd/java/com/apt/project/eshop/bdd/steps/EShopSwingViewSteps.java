bcb15eeeb034346e3bc8e19948a393506d0f01d6
Giovanni <giovanniburbi10@gmail.com>
2021-10-07 17:22:35 +0200

    Then step scenario 5 completed

--- com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/EShopSwingViewSteps.java
+++ com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/EShopSwingViewSteps.java
@@ -109,6 +109,10 @@ public class EShopSwingViewSteps {
 
 	@Then("The cart list contains an element with the following values")
 	public void the_cart_list_contains_an_element_with_the_following_values(List<Map<String, String>> values) {
-		throw new io.cucumber.java.PendingException();	
+		values.forEach(
+		    v -> assertThat(window.list("cartList").contents())
+		    		.anySatisfy(e -> assertThat(e)
+		    			.contains(new Product(v.get("id"), v.get("name"), Double.parseDouble(v.get("price")), Integer.parseInt(v.get("quantity"))).toStringExtended()))
+		);
 	}
 }
