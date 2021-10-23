6508de95c90464b126244d0321a612c2d62b79f0
Giovanni <giovanniburbi10@gmail.com>
2021-10-23 15:45:48 +0200

    Fix: removed useless flag from ShopManager

--- com.apt.project.eshop/src/main/java/com/apt/project/eshop/repository/ShopManager.java
+++ com.apt.project.eshop/src/main/java/com/apt/project/eshop/repository/ShopManager.java
@@ -12,28 +12,20 @@ public class ShopManager {
 	public ShopManager(TransactionManager transactionManager) {
 		this.transactionManager = transactionManager;
 	}
-	
+
 	public void checkout() {
-		transactionManager.doInTransaction(
-			productRepository -> {
-				boolean outOfStockFailure = false;
-				List<Product> products = productRepository.allCart();
-				try {
-					for (Product product : products) {
-						productRepository.removeFromStorage(product);
-					} 
-				} catch (RepositoryException e) {
-					outOfStockFailure = true;
-					throw new MongoException("Insufficient stock");
+		transactionManager.doInTransaction(productRepository -> {
+			List<Product> products = productRepository.allCart();
+			try {
+				for (Product product : products) {
+					productRepository.removeFromStorage(product);
 				}
-				if(!outOfStockFailure) {
-					products.stream().forEach(productRepository::removeFromCart);
-				}
-				return null;
-			}		
-		);
+			} catch (RepositoryException e) {
+				throw new MongoException("Insufficient stock");
+			}
+			products.stream().forEach(productRepository::removeFromCart);
+			return null;
+		});
 	}
-	
-	
-	
+
 }
