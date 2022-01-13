09480b9805acb6cb97de96f69ab1272aaa8fa399
Giovanni <giovanniburbi10@gmail.com>
2022-01-13 17:03:08 +0100

    Removed unused import

--- com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/EShopSwingViewSteps.java
+++ com.apt.project.eshop/src/bdd/java/com/apt/project/eshop/bdd/steps/EShopSwingViewSteps.java
@@ -3,7 +3,6 @@ package com.apt.project.eshop.bdd.steps;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
